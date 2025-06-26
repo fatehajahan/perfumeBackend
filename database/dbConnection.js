1a809a3dc21f449b5325167d043eb6327842ebcb
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-06-26 20:32:54 +0600

    done

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function dbConnection() {
-    mongoose.connect(`mongodb+srv://${process.env.DBUSER_NAME}:${process.env.DBUSER_PASSWORD}@cluster0.liaz7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
+    mongoose.connect(`${process.env.DB_URL}`)
     .then(() => console.log('db conneted'));
 }
 
