78533dfcff216e1be58051106fc9782ee7a5c148
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-06-27 19:12:06 +0600

    done

--- route/api/index.js
+++ route/api/index.js
@@ -3,7 +3,7 @@ const route = express.Router()
 const authRout = require("./authentication")
 const categoryRoute = require("./category")
 const subCategoryRoute = require("./subCategory")
-const productRoute = require("./order")
+const productRoute = require("./product")
 const orderRoute = require("./order")
 
 route.use("/authentication", authRout)
