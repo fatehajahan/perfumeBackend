f30a5c6a00ccfed6cfc9ab22f546d57ec5a16112
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-09-27 23:30:03 +0600

    done

--- route/api/product.js
+++ route/api/product.js
@@ -3,16 +3,6 @@ const { productCtrl, getAllProductCtrl, deleteProduct, updateSingleProductCtrl, 
 const multer = require('multer')
 const route = express.Router()
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './upload')
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//         cb(null, file.fieldname + '-' + uniqueSuffix + `.${file.originalname.split(".")[1]}`)
-//         console.log(file.originalname.split(".")[1])
-//     }
-// })
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage })
 
