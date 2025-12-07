b6d769af349668aade7050e2fc5c8f2b565a942a
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-12-07 19:59:53 +0600

    done

--- helpers/emailVarification.js
+++ helpers/emailVarification.js
@@ -1,25 +1,27 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 async function emailVarification(email, otp) {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        },
+            user: process.env.MAILTRAP_USER,
+            pass: process.env.MAILTRAP_PASS
+        }
     });
 
     try {
         const info = await transporter.sendMail({
-            from: process.env.EMAIL_USER,
+            from: "perfume@mailtrap.io",
             to: email,
             subject: "Perfume: OTP Verification",
             text: `Your OTP is ${otp}`
         });
+
         console.log("Email sent:", info.response);
     } catch (err) {
         console.log("Nodemailer error:", err);
     }
 }
 
-module.exports = emailVarification
+module.exports = emailVarification;
